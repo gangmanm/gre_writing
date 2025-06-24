926f860ab5ee9f3b7f84e1c03cbfad3fd5e66f16
Do Kyung Lee <97601109+gangmanm@users.noreply.github.com>
2025-06-24 15:37:52 +0900

    [feat] gitpage publish

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/gre_writing/',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'favicon.svg') {
+            return 'favicon.svg';
+          }
+          return 'assets/[name]-[hash][extname]';
+        }
+      }
+    }
+  }
 })
