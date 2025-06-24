bfbea5e344c3e9655af22021e657d3ad0dbf7e3f
Do Kyung Lee <97601109+gangmanm@users.noreply.github.com>
2025-06-24 15:39:33 +0900

    [feat] github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/gre_writing/',
   build: {
+    outDir: 'dist',
     assetsDir: 'assets',
+    copyPublicDir: true,
     rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      },
       output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
         assetFileNames: (assetInfo) => {
-          if (assetInfo.name === 'favicon.svg') {
-            return 'favicon.svg';
+          const info = assetInfo.name.split('.')
+          const ext = info[info.length - 1]
+          if (ext === 'svg') {
+            return `[name][extname]`
           }
-          return 'assets/[name]-[hash][extname]';
+          return 'assets/[name].[hash][extname]'
         }
       }
+    }
+  },
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript'
     }
   }
 })
